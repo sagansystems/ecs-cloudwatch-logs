2528eae06883c00fe85045294cc869802c90371b
Matt Baker <matt@sagansystems.com>
2015-06-09 17:23:49 -0700

    Copying start.sh into image

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-EC2_INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
+EC2_INSTANCE_ID=`curl -m 20 http://169.254.169.254/latest/meta-data/instance-id`
 
 sed -i "s/EC2_INSTANCE_ID/${EC2_INSTANCE_ID}/" /etc/rsyslog.conf
 
